543c1d08c228100c6f899f7cc8a0e7c80b266e07
Beatriz Mendes <beatriz.mira.mendes@tecnico.ulisboa.pt>
2025-10-22 16:26:16 +0100

    smaller dots

--- dashboard/idioms/map.js
+++ dashboard/idioms/map.js
@@ -26,7 +26,7 @@ const MapModule = (function() {
         .range(['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b',
                 '#e377c2','#7f7f7f','#bcbd22','#17becf','#aec7e8','#ffbb78']);
 
-    const baseRadius = 4.2;
+    const baseRadius = 3.5;
     const baseStroke = 0.3;
 
     function getColorByFur(fur) {
